186f562ca084490dee3d8df18df799be5967c9f1
Sidney3 <sidneyarthurlevine1@hotmail.com>
2025-02-22 17:20:51 -0500

    pricing engine interface

---
+++ src/pricing_engine.py
@@ -0,0 +1,19 @@
+from __future__ import annotations
+
+import pandas as pd
+from gt_trading_client import SharedState
+
+class PricingEngine:
+    def __init__(self, shared_state: SharedState, historical_data: pd.DataFrame):
+        """
+        The format of the historical data:
+
+        a time series dataframe (indexed by tick) with columns for
+        each symbol.
+
+        historical_data.iloc[0]["AAPL"] will give the price of 
+        "AAPL" at tick 0
+        """
+        pass
+    def fair_values(self) -> dict[str, float]:
+        return {}
